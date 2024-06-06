6054ed017564a8332ea23fb423be1e3bcb00e65b
SharieRhea <sharierhea@gmail.com>
2024-06-06 10:49:27 -0400

    Refactoring and updates API requests with better parameter syntax.

--- TrackTagger.py
+++ TrackTagger.py
@@ -3,7 +3,6 @@ import os
 import customtkinter as ctk
 import requests
 from io import BytesIO
-import urllib.parse
 import json
 import music_tag
 from WelcomePage import WelcomePage
@@ -14,7 +13,7 @@ from AlbumConfirmation import AlbumConfirmation
 from SearchAlbum import SearchAlbum
 from AlbumSelection import AlbumSelection
 
-ENDPOINT = "https://ws.audioscrobbler.com/2.0/?method="
+ENDPOINT = "https://ws.audioscrobbler.com/2.0/"
 
 def main():
     load_dotenv()
@@ -39,79 +38,100 @@ class Application(ctk.CTk):
 
     def display_welcome_page(self):
         """Displays a frame to collect the directory path and tag lists."""
-        self.welcome_page = WelcomePage(self, self.on_click_continue_welcome_page)
-        self.welcome_page.grid(row = 0, column = 0, padx = 20, pady = 20, sticky = "ew")
+        welcome_page = WelcomePage(self, lambda: self.on_click_continue_welcome_page(welcome_page))
+        welcome_page.grid(row = 0, column = 0, padx = 20, pady = 20, sticky = "ew")
 
-    def on_click_continue_welcome_page(self):
+    def on_click_continue_welcome_page(self, welcome_page):
         """Collects data from welcome page and begins processing data."""
-        self.directory_path = self.welcome_page.get_directory_path()
-        self.allowed_tags = self.welcome_page.get_allowed_tags()
-        self.denied_tags = self.welcome_page.get_denied_tags()
-        self.welcome_page.destroy()
+        self.directory_path = welcome_page.get_directory_path()
+        self.allowed_tags = welcome_page.get_allowed_tags()
+        self.denied_tags = welcome_page.get_denied_tags()
+        welcome_page.destroy()
 
-        # begin processing every song in the directory
+        # add all files to a list to be processed
+        self.song_list = []
+        self.song_index = 0
         for file in os.scandir(self.directory_path):
             if not file.is_file() or file.name[-4:] != ".mp3":
                 continue
 
-            info = file.name.split(" - ")
-            self.filepath = file.path
-            self.process_song(info[0], info[1][:-4])
+            self.song_list.append(file)
 
-    def on_click_continue_search_track(self):
+        self.process_song(None)
+
+    def on_click_continue_search_track(self, search_track):
         """Collects data from the search track page and begins processing a song."""
-        self.title_search = self.search_track.get_title()
-        self.artist_search = self.search_track.get_artist()
-        self.process_song(self.title_search, self.artist_search)
+        self.process_song(search_track)
 
-    def on_click_yes_track_confirmation(self):
+    def on_click_yes_track_confirmation(self, track_confirmation):
         """Destroys the track confirmation dialog and sets up tag dialog."""
-        self.track_confirmation.destroy()
-        self.tag_selection = TagSelection(self, self.title, self.artist, self.tags, self.allowed_tags, self.denied_tags, self.on_click_continue_tag_selection)
-        self.tag_selection.grid(row = 0, column = 0, padx = 20, pady = 20, sticky = "ew")
+        track_confirmation.destroy()
+        tag_selection = TagSelection(self, self.title, self.artist, self.tags, self.allowed_tags, self.denied_tags, lambda: self.on_click_continue_tag_selection(tag_selection))
+        tag_selection.grid(row = 0, column = 0, padx = 20, pady = 20, sticky = "ew")
 
-    def on_click_no_track_confirmation(self):
+    def on_click_no_track_confirmation(self, title_search, artist_search, track_confirmation):
         """Destroys the track confirmation dialog and moves to search track dialog."""
-        self.track_confirmation.destroy()
-        self.search_track = SearchTrack(self, self.title_search, self.artist_search, None)
-        self.search_track.grid(row = 0, column = 0, padx = 20, pady = 20, sticky = "ew")
+        track_confirmation.destroy()
+        search_track = SearchTrack(self, title_search, artist_search, lambda: self.on_click_continue_search_track(search_track))
+        search_track.grid(row = 0, column = 0, padx = 20, pady = 20, sticky = "ew")
 
-    def on_click_continue_tag_selection(self):
+    def on_click_continue_tag_selection(self, tag_selection):
         """Collects data from the tag selection dialog and proceeds to album selection."""
-        self.tags = self.tag_selection.get_selected_tags()
-        self.tag_selection.destroy()
+        self.tags = tag_selection.get_selected_tags()
+        tag_selection.destroy()
 
         if self.album_found:
-            self.album_confirmation = AlbumConfirmation(self, self.title, self.artist, self.album_title, self.album_artist, self.cover, self.on_click_yes_album_confirmation, self.on_click_no_album_confirmation)
-            self.album_confirmation.grid(row = 0, column = 0, padx = 20, pady = 20)
+            album_confirmation = AlbumConfirmation(
+                self,
+                self.title,
+                self.artist,
+                self.album_title,
+                self.album_artist,
+                self.cover,
+                lambda: self.on_click_yes_album_confirmation(album_confirmation),
+                lambda: self.on_click_no_album_confirmation(album_confirmation)
+            )
+            album_confirmation.grid(row = 0, column = 0, padx = 20, pady = 20)
         else:
-            self.album_search = SearchAlbum(self, self.title, self.artist, self.on_click_continue_album_search)
-            self.album_search.grid(row = 0, column = 0, padx = 20, pady = 20)
+            album_search = SearchAlbum(self, self.title, self.artist, lambda: self.on_click_continue_album_search(album_search))
+            album_search.grid(row = 0, column = 0, padx = 20, pady = 20)
 
-    def on_click_yes_album_confirmation(self):
-        self.album_confirmation.destroy()
+    def on_click_yes_album_confirmation(self, album_confirmation):
+        album_confirmation.destroy()
+        self.write_out_metadata()
 
-        self.write_out_metadata(self.filepath, self.title, self.artist, self.album_title, self.album_artist, self.cover, self.tags)
+    def on_click_no_album_confirmation(self, album_confirmation):
+        album_confirmation.destroy()
+        album_search = SearchAlbum(self, self.title, self.artist, lambda: self.on_click_continue_album_search(album_search))
+        album_search.grid(row = 0, column = 0, padx = 20, pady = 20)
 
-    def on_click_no_album_confirmation(self):
-        self.album_confirmation.destroy()
-        self.album_search = SearchAlbum(self, self.title, self.artist, self.on_click_continue_album_search)
-        self.album_search.grid(row = 0, column = 0, padx = 20, pady = 20)
+    def on_click_continue_album_search(self, album_search):
+        album_title_search = album_search.get_title()
+        album_search.destroy()
 
-    def on_click_continue_album_search(self):
-        album_title_search = self.album_search.get_title()
-        self.album_search.destroy()
-
-        info = requests.get(f"{ENDPOINT}album.search&api_key={self.key}&album={urllib.parse.quote_plus(album_title_search)}&format=json")
+        parameters = {
+            "method": "album.search",
+            "api_key": self.key,
+            "album": album_title_search,
+            "format": "json"
+        }
+        info = requests.get(ENDPOINT, params = parameters)
         album_data = json.loads(info.text)
         self.albums = album_data["results"]["albummatches"]["album"]
 
-        self.album_selection = AlbumSelection(self, self.title, self.artist, self.albums, self.on_click_continue_album_selection, self.on_click_back_album_selection)
-        self.album_selection.grid(row = 0, column = 0, padx = 20, pady = 20)
+        album_selection = AlbumSelection(
+            self, 
+            self.title, 
+            self.artist, 
+            self.albums, 
+            lambda: self.on_click_continue_album_selection(album_selection), 
+            lambda: self.on_click_back_album_selection(album_selection)
+        )
+        album_selection.grid(row = 0, column = 0, padx = 20, pady = 20)
 
-    def on_click_continue_album_selection(self):
-        self.album_index = self.album_selection.get_album_index()
-        self.album_selection.destroy()
+    def on_click_continue_album_selection(self, album_selection):
+        self.album_index = album_selection.get_album_index()
+        album_selection.destroy()
 
         self.album_title = self.albums[self.album_index]["name"]
         self.album_artist = self.albums[self.album_index]["artist"]
@@ -120,24 +140,41 @@ class Application(ctk.CTk):
         response = requests.get(album_image_url)
         self.cover = response.content
 
-        self.write_out_metadata(self.filepath, self.title, self.artist, self.album_title, self.album_artist, self.cover, self.tags)
+        self.write_out_metadata()
 
-    def on_click_back_album_selection(self):
-        self.album_selection.destroy()
+    def on_click_back_album_selection(self, album_selection):
+        album_selection.destroy()
 
-        self.album_search = SearchAlbum(self, self.title, self.artist, self.on_click_continue_album_search)
-        self.album_search.grid(row = 0, column = 0, padx = 20, pady = 20)
+        album_search = SearchAlbum(self, self.title, self.artist, lambda: self.on_click_continue_album_search(album_search))
+        album_search.grid(row = 0, column = 0, padx = 20, pady = 20)
 
-    def process_song(self, title_search, artist_search):
+    def process_song(self, track_search):
         """Uses the last.fm API to search for track info based on title and artist."""
-        # todo: replace this with the better dictionary syntax for parameters
-        info = requests.get(f"{ENDPOINT}track.getInfo&api_key={self.key}&track={urllib.parse.quote_plus(title_search)}&artist={urllib.parse.quote_plus(artist_search)}&format=json")
+
+        if track_search is None:
+            self.filepath = self.song_list[self.song_index].path
+            filename = self.song_list[self.song_index].name
+            title_search = filename.split(" - ")[0]
+            artist_search = filename.split(" - ")[1][:-4]
+        else:
+            title_search = track_search.get_title()
+            artist_search = track_search.get_artist()
+            track_search.destroy()
+
+        parameters = {
+            "method": "track.getInfo",
+            "api_key": self.key,
+            "track": title_search,
+            "artist": artist_search,
+            "format": "json"
+        }
+        info = requests.get(ENDPOINT, params = parameters)
         data = json.loads(info.text)
 
         if "track" not in data:
             # no track found, must search 
-            self.search_track = SearchTrack(self, title_search, artist_search, None)
-            self.search_track.grid(row = 0, column = 0, padx = 20, pady = 20, sticky = "ew")
+            search_track = SearchTrack(self, title_search, artist_search, lambda: self.on_click_continue_search_track(search_track))
+            search_track.grid(row = 0, column = 0, padx = 20, pady = 20, sticky = "ew")
             return
 
         self.title = data["track"]["name"]
@@ -159,18 +196,28 @@ class Application(ctk.CTk):
             self.album_found = False
 
         # display confirmation page for this track
-        self.track_confirmation = TrackConfirmation(self, self.title, self.artist, playcount, self.on_click_yes_track_confirmation, self.on_click_no_track_confirmation)
-        self.track_confirmation.grid(row = 0, column = 0, padx = 20, pady = 20, sticky = "ew")
+        track_confirmation = TrackConfirmation(
+            self, 
+            self.title, 
+            self.artist, 
+            playcount, 
+            lambda: self.on_click_yes_track_confirmation(track_confirmation), 
+            lambda: self.on_click_no_track_confirmation(title_search, artist_search, track_confirmation)
+        )
+        track_confirmation.grid(row = 0, column = 0, padx = 20, pady = 20, sticky = "ew")
 
-    def write_out_metadata(self, filepath, title, artist, album_title, album_artist, cover, tags):
-        file: dict[str, str | bytes] = music_tag.load_file(filepath)
-        file["title"] = title
-        file["artist"] = artist
-        file["album"] = album_title
-        file["albumartist"] = album_artist
-        file["artwork"] = BytesIO(cover).read()
-        file["genre"] = tags
+    def write_out_metadata(self):
+        file: dict[str, str | bytes] = music_tag.load_file(self.filepath)
+        file["title"] = self.title
+        file["artist"] = self.artist
+        file["album"] = self.album_title
+        file["albumartist"] = self.album_artist
+        file["artwork"] = BytesIO(self.cover).read()
+        file["genre"] = self.tags
         file.save()
+
+        self.song_index = self.song_index + 1
+        self.process_song(None)
 
 if __name__ == "__main__":
     main()
